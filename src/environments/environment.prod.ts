0f6081ce42299ab338b7704d55f8f2d6277c642c
FelipeBG1 <felibustosomg@gmail.com>
2021-09-30 11:45:53 -0300

    ModeloParcialparte3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD3hNdY8PGJfXBPZazhM61hxQKTJzWQ1HI",
+    authDomain: "modeloparcial-9b0cb.firebaseapp.com",
+    projectId: "modeloparcial-9b0cb",
+    storageBucket: "modeloparcial-9b0cb.appspot.com",
+    messagingSenderId: "628565345942",
+    appId: "1:628565345942:web:08188a90a0e3e57305e4b7"
+  }
 };
